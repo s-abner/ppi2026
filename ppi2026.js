01e2ca3e4ba7ea5b0d2fc59609201616cf7df288
s-abner <samuel.abner@escolar.ifrn.edu.br>
2026-05-07 13:37:30 +0000

    Aula 07/05

--- ppi2026.js
+++ ppi2026.js
@@ -8,7 +8,6 @@
 
 // Aula 06/05 - Introdução à JavaScript
 
-
 console.log('Bem vindo a PPI!');
 
 // variáveis
@@ -36,3 +35,120 @@ console.log('5' === 5); // Compara tipo E valor
 console.log(x++);
 console.log(x);
 console.log(++x);
+
+// x++;
+// ++x;
+// x += 1;
+// x = x + 1;
+
+// Operadores lógicos && (AND) || (OR) ! (NOT)
+let isExpression = true && 5+2*3 < 10 || false;
+console.log(isExpression);
+
+// if...else
+// if (condition) {
+//     // condition === true
+// } else {
+//     // condition === false
+// }
+
+// Template Strings
+let text = `React é 'tudo de bom!'
+Programar é minha vida!/
+Eu amo o IFRN!`;
+console.log(text);
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// Array
+let fruits = ['Banana', 'Orange', 'Apple', 'Mango'];
+
+console.log(fruits.length); // Tamanho
+console.log(fruits[0])
+
+// Insere um elemento no final do array
+fruits.push('Kiwi');
+console.log(fruits);
+
+// Remove e retorna o último elemento do array
+console.log(fruits.pop());
+console.log(fruits);
+
+// Remove e retorna o primeiro elemento do array
+console.log(fruits.shift());
+console.log(fruits);
+
+// Insere um elemento no começo do array
+fruits.unshift('Lemon');
+console.log(fruits);
+
+// Argumento 1 - posição
+// Argumento 2 - qts elementos serão removidos
+// Argumento 3 - lista de elementos que serão inseridos
+fruits.splice(2, 0, 'Kiwi', 'Blueberry');
+console.log(fruits);
+
+fruits.splice(2, 1);
+console.log(fruits);
+
+// Desafio - substitui 'Apple' por 'Kiwi'
+fruits.splice(3, 1, 'Kiwi');
+console.log(fruits);
+
+// Busca índice do elemento 'Kiwi'
+let index = fruits.indexOf('Kiwi');
+console.log(index);
+
+fruits.splice(index, 1, 'Apple');
+console.log(fruits);
+
+// Ordem crescente
+fruits.sort(); // toSorted()
+console.log(fruits);
+
+// Ordem decrescente
+fruits.reverse(); // toReversed()
+console.log(fruits);
+
+console.log('Sort/Reversed')
+let fruitsSort = fruits.toSorted(); // Não altera o array original
+console.log(fruits);
+console.log(fruitsSort);
+
+let fruitsReverse = fruits.toReversed(); // Não altera o array original
+console.log(fruits);
+console.log(fruitsReverse);
+
+const numbers = [45, 4, 9, 16, 25];
+console.log(numbers);
+
+// Desafio - criar um array 'numbers2' que dupliquem os valores de 'numbers'
+// Output: [90, 8, 18, 32, 50]
+const numbers2 = [];
+for (let i = 0; i < numbers.length; i++) {
+    numbers2.push(numbers[i]*2);    
+}
+console.log(numbers);
+console.log(numbers2);
+
+function myFunction(value, index, array) {
+    return value * 2;
+}
+
+const numberMap = numbers.map(myFunction);
+console.log(numberMap);
+
+console.log(numbers.map( (number) => number*2 ));
+
+console.log(numbers.toSorted((a, b) => a - b)); // Ordem crescente
+console.log(numbers.toSorted((a, b) => b - a)); // Ordem decrescente
+
+
+numbers.sort((a, b) => a - b);
+// Maior valor
+console.log(numbers[numbers.length - 1]);
+// Menor valor
+console.log(numbers[0]);
